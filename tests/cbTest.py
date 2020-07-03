9ce68e002530f70ee289724151e778e3df07de62
Tulesha <tulesha_xochet_pit@mail.ru>
2020-07-03 16:59:34 +0300

    Correct tests

--- tests/cbTest.py
+++ tests/cbTest.py
@@ -16,82 +16,96 @@ class TestCbMethods(unittest.TestCase):
         number = random.random()
         self.assertEqual(number, str_to_float(f'{number}'))
 
-    def test_get_rates_usd(self):
+    def test_get_rates_usd_1(self):
         bank_1 = CbBank(datetime.date(2002, 3, 2))
         usd = bank_1.get_rates_usd()
         self.assertEqual('USD', usd.name)
         self.assertEqual(30.9436, usd.rate)
 
+    def test_get_rates_usd_2(self):
         bank_2 = CbBank(datetime.date(2015, 7, 27))
         usd = bank_2.get_rates_usd()
         self.assertEqual('USD', usd.name)
         self.assertEqual(58.0374, usd.rate)
 
+    def test_get_rates_usd_3(self):
         bank_3 = CbBank(datetime.date(2020, 5, 30))
         usd = bank_3.get_rates_usd()
         self.assertEqual('USD', usd.name)
         self.assertEqual(70.7520, usd.rate)
 
+    def test_get_rates_usd_error(self):
         bank_4 = CbBank(datetime.date(1950, 1, 27))
         with self.assertRaises(CentralBankError):
             bank_4.get_rates_usd()
 
-    def test_get_rates_eur(self):
-        bank = CbBank(datetime.date(2002, 3, 2))
-        eur = bank.get_rates_eur()
+    def test_get_rates_eur_1(self):
+        bank_1 = CbBank(datetime.date(2002, 3, 2))
+        eur = bank_1.get_rates_eur()
         self.assertEqual('EUR', eur.name)
         self.assertEqual(26.8343, eur.rate)
 
+    def test_get_rates_eur_2(self):
         bank_2 = CbBank(datetime.date(2015, 7, 27))
         eur = bank_2.get_rates_eur()
         self.assertEqual('EUR', eur.name)
         self.assertEqual(63.6090, eur.rate)
 
+    def test_get_rates_eur_3(self):
         bank_3 = CbBank(datetime.date(2020, 5, 30))
         eur = bank_3.get_rates_eur()
         self.assertEqual('EUR', eur.name)
         self.assertEqual(78.5489, eur.rate)
 
+    def test_get_rates_eur_error(self):
         bank_4 = CbBank(datetime.date(1950, 1, 27))
         with self.assertRaises(CentralBankError):
             bank_4.get_rates_eur()
 
-    def test_get_rates_gbp(self):
-        bank = CbBank(datetime.date(2002, 3, 2))
-        gbp = bank.get_rates_gbp()
+    def test_get_rates_gbp_1(self):
+        bank_1 = CbBank(datetime.date(2002, 3, 2))
+        gbp = bank_1.get_rates_gbp()
         self.assertEqual('GBP', gbp.name)
         self.assertEqual(43.8254, gbp.rate)
 
+    def test_get_rates_gbp_2(self):
         bank_2 = CbBank(datetime.date(2015, 7, 27))
         gbp = bank_2.get_rates_gbp()
         self.assertEqual('GBP', gbp.name)
         self.assertEqual(89.8825, gbp.rate)
 
+    def test_get_rates_gbp_3(self):
         bank_3 = CbBank(datetime.date(2020, 5, 30))
         gbp = bank_3.get_rates_gbp()
         self.assertEqual('GBP', gbp.name)
         self.assertEqual(87.0603, gbp.rate)
 
+    def test_get_rates_gbp_error(self):
         bank_4 = CbBank(datetime.date(1950, 1, 27))
         with self.assertRaises(CentralBankError):
             bank_4.get_rates_gbp()
 
-    def test_get_rates_jpy(self):
-        bank = CbBank(datetime.date(2002, 3, 2))
-        jpy = bank.get_rates_jpy()
+    def test_get_rates_jpy_1(self):
+        bank_1 = CbBank(datetime.date(2002, 3, 2))
+        jpy = bank_1.get_rates_jpy()
         self.assertEqual('JPY', jpy.name)
         self.assertEqual(23.1527, jpy.rate)
 
+    def test_get_rates_jpy_2(self):
         bank_2 = CbBank(datetime.date(2015, 7, 27))
         jpy = bank_2.get_rates_jpy()
         self.assertEqual('JPY', jpy.name)
         self.assertEqual(46.8044, jpy.rate)
 
+    def test_get_rates_jpy_3(self):
         bank_3 = CbBank(datetime.date(2020, 5, 30))
         jpy = bank_3.get_rates_jpy()
         self.assertEqual('JPY', jpy.name)
         self.assertEqual(65.9539, jpy.rate)
 
+    def test_get_rates_jpy_error(self):
         bank_4 = CbBank(datetime.date(1950, 1, 27))
         with self.assertRaises(CentralBankError):
             bank_4.get_rates_jpy()
+
+
--- tests/myFinTest.py
+++ tests/myFinTest.py
@@ -13,35 +13,79 @@ class TestMyfinMethods(unittest.TestCase):
         with self.assertRaises(MyFinBankError):
             parser_HTML('fgvbhnjk', 'cfvgbhnjmk')
 
-    def test_get_rate_type(self):
+    def test_get_rate_usd_bank_name_type(self):
         bank_1 = Myfin('usd', 'moskva')
         for bank in bank_1.get_rate():
             self.assertTrue(isinstance(bank.bank_name, str))
+
+    def test_get_rate_usd_rate_buy_type(self):
+        bank_1 = Myfin('usd', 'moskva')
+        for bank in bank_1.get_rate():
             self.assertTrue(isinstance(bank.rate_buy, float))
+
+    def test_get_rate_usd_rate_sell_type(self):
+        bank_1 = Myfin('usd', 'moskva')
+        for bank in bank_1.get_rate():
             self.assertTrue(isinstance(bank.rate_sell, float))
 
-        bank_2 = Myfin('eur', 'moskva')
-        for bank in bank_2.get_rate():
+    def test_get_rate_eur_bank_name_type(self):
+        bank_1 = Myfin('eur', 'moskva')
+        for bank in bank_1.get_rate():
             self.assertTrue(isinstance(bank.bank_name, str))
+
+    def test_get_rate_eur_rate_buy_type(self):
+        bank_1 = Myfin('eur', 'moskva')
+        for bank in bank_1.get_rate():
             self.assertTrue(isinstance(bank.rate_buy, float))
+
+    def test_get_rate_eur_rate_sell_type(self):
+        bank_1 = Myfin('eur', 'moskva')
+        for bank in bank_1.get_rate():
             self.assertTrue(isinstance(bank.rate_sell, float))
 
-        bank_3 = Myfin('gbp', 'moskva')
-        for bank in bank_3.get_rate():
+    def test_get_rate_gbp_bank_name_type(self):
+        bank_1 = Myfin('gbp', 'moskva')
+        for bank in bank_1.get_rate():
             self.assertTrue(isinstance(bank.bank_name, str))
+
+    def test_get_rate_gbp_rate_buy_type(self):
+        bank_1 = Myfin('gbp', 'moskva')
+        for bank in bank_1.get_rate():
             self.assertTrue(isinstance(bank.rate_buy, float))
+
+    def test_get_rate_gbp_rate_sell_type(self):
+        bank_1 = Myfin('gbp', 'moskva')
+        for bank in bank_1.get_rate():
             self.assertTrue(isinstance(bank.rate_sell, float))
 
-        bank_4 = Myfin('jpy', 'moskva')
-        for bank in bank_4.get_rate():
+    def test_get_rate_jpy_bank_name_type(self):
+        bank_1 = Myfin('jpy', 'moskva')
+        for bank in bank_1.get_rate():
             self.assertTrue(isinstance(bank.bank_name, str))
+
+    def test_get_rate_jpy_rate_buy_type(self):
+        bank_1 = Myfin('jpy', 'moskva')
+        for bank in bank_1.get_rate():
             self.assertTrue(isinstance(bank.rate_buy, float))
+
+    def test_get_rate_jpy_rate_sell_type(self):
+        bank_1 = Myfin('jpy', 'moskva')
+        for bank in bank_1.get_rate():
             self.assertTrue(isinstance(bank.rate_sell, float))
 
-        bank_5 = Myfin('cny', 'moskva')
-        for bank in bank_5.get_rate():
+    def test_get_rate_cny_bank_name_type(self):
+        bank_1 = Myfin('cny', 'moskva')
+        for bank in bank_1.get_rate():
             self.assertTrue(isinstance(bank.bank_name, str))
+
+    def test_get_rate_cny_rate_buy_type(self):
+        bank_1 = Myfin('cny', 'moskva')
+        for bank in bank_1.get_rate():
             self.assertTrue(isinstance(bank.rate_buy, float))
+
+    def test_get_rate_cny_rate_sell_type(self):
+        bank_1 = Myfin('cny', 'moskva')
+        for bank in bank_1.get_rate():
             self.assertTrue(isinstance(bank.rate_sell, float))
 
     def test_myFin_constructor_error(self):
