489baeb545038a4019389f2df70d7f5f062580e2
Tulesha <tulesha_xochet_pit@mail.ru>
2020-07-03 20:09:32 +0300

    Correct tests

--- tests/cbTest.py
+++ tests/cbTest.py
@@ -16,22 +16,34 @@ class TestCbMethods(unittest.TestCase):
         number = random.random()
         self.assertEqual(number, str_to_float(f'{number}'))
 
-    def test_get_rates_usd_1(self):
+    def test_get_rates_usd_curr_name_1(self):
         bank_1 = CbBank(datetime.date(2002, 3, 2))
         usd = bank_1.get_rates_usd()
         self.assertEqual('USD', usd.name)
+
+    def test_get_rates_usd_rate_1(self):
+        bank_1 = CbBank(datetime.date(2002, 3, 2))
+        usd = bank_1.get_rates_usd()
         self.assertEqual(30.9436, usd.rate)
 
-    def test_get_rates_usd_2(self):
+    def test_get_rates_usd_curr_name_2(self):
         bank_2 = CbBank(datetime.date(2015, 7, 27))
         usd = bank_2.get_rates_usd()
         self.assertEqual('USD', usd.name)
+
+    def test_get_rates_usd_rate_2(self):
+        bank_2 = CbBank(datetime.date(2015, 7, 27))
+        usd = bank_2.get_rates_usd()
         self.assertEqual(58.0374, usd.rate)
 
-    def test_get_rates_usd_3(self):
+    def test_get_rates_usd_curr_name_3(self):
         bank_3 = CbBank(datetime.date(2020, 5, 30))
         usd = bank_3.get_rates_usd()
         self.assertEqual('USD', usd.name)
+
+    def test_get_rates_usd_rate_3(self):
+        bank_3 = CbBank(datetime.date(2020, 5, 30))
+        usd = bank_3.get_rates_usd()
         self.assertEqual(70.7520, usd.rate)
 
     def test_get_rates_usd_error(self):
@@ -39,22 +51,34 @@ class TestCbMethods(unittest.TestCase):
         with self.assertRaises(CentralBankError):
             bank_4.get_rates_usd()
 
-    def test_get_rates_eur_1(self):
+    def test_get_rates_eur_curr_name_1(self):
         bank_1 = CbBank(datetime.date(2002, 3, 2))
         eur = bank_1.get_rates_eur()
         self.assertEqual('EUR', eur.name)
+
+    def test_get_rates_eur_rate_1(self):
+        bank_1 = CbBank(datetime.date(2002, 3, 2))
+        eur = bank_1.get_rates_eur()
         self.assertEqual(26.8343, eur.rate)
 
-    def test_get_rates_eur_2(self):
+    def test_get_rates_eur_curr_name_2(self):
         bank_2 = CbBank(datetime.date(2015, 7, 27))
         eur = bank_2.get_rates_eur()
         self.assertEqual('EUR', eur.name)
+
+    def test_get_rates_eur_rate_2(self):
+        bank_2 = CbBank(datetime.date(2015, 7, 27))
+        eur = bank_2.get_rates_eur()
         self.assertEqual(63.6090, eur.rate)
 
-    def test_get_rates_eur_3(self):
+    def test_get_rates_eur_curr_name_3(self):
         bank_3 = CbBank(datetime.date(2020, 5, 30))
         eur = bank_3.get_rates_eur()
         self.assertEqual('EUR', eur.name)
+
+    def test_get_rates_eur_rate_3(self):
+        bank_3 = CbBank(datetime.date(2020, 5, 30))
+        eur = bank_3.get_rates_eur()
         self.assertEqual(78.5489, eur.rate)
 
     def test_get_rates_eur_error(self):
@@ -62,22 +86,34 @@ class TestCbMethods(unittest.TestCase):
         with self.assertRaises(CentralBankError):
             bank_4.get_rates_eur()
 
-    def test_get_rates_gbp_1(self):
+    def test_get_rates_gbp_curr_name_1(self):
         bank_1 = CbBank(datetime.date(2002, 3, 2))
         gbp = bank_1.get_rates_gbp()
         self.assertEqual('GBP', gbp.name)
+
+    def test_get_rates_gbp_rate_1(self):
+        bank_1 = CbBank(datetime.date(2002, 3, 2))
+        gbp = bank_1.get_rates_gbp()
         self.assertEqual(43.8254, gbp.rate)
 
-    def test_get_rates_gbp_2(self):
+    def test_get_rates_gbp_curr_name_2(self):
         bank_2 = CbBank(datetime.date(2015, 7, 27))
         gbp = bank_2.get_rates_gbp()
         self.assertEqual('GBP', gbp.name)
+
+    def test_get_rates_gbp_rate_2(self):
+        bank_2 = CbBank(datetime.date(2015, 7, 27))
+        gbp = bank_2.get_rates_gbp()
         self.assertEqual(89.8825, gbp.rate)
 
-    def test_get_rates_gbp_3(self):
+    def test_get_rates_gbp_curr_name_3(self):
         bank_3 = CbBank(datetime.date(2020, 5, 30))
         gbp = bank_3.get_rates_gbp()
         self.assertEqual('GBP', gbp.name)
+
+    def test_get_rates_gbp_rate_3(self):
+        bank_3 = CbBank(datetime.date(2020, 5, 30))
+        gbp = bank_3.get_rates_gbp()
         self.assertEqual(87.0603, gbp.rate)
 
     def test_get_rates_gbp_error(self):
@@ -85,27 +121,37 @@ class TestCbMethods(unittest.TestCase):
         with self.assertRaises(CentralBankError):
             bank_4.get_rates_gbp()
 
-    def test_get_rates_jpy_1(self):
+    def test_get_rates_jpy_curr_name_1(self):
         bank_1 = CbBank(datetime.date(2002, 3, 2))
         jpy = bank_1.get_rates_jpy()
         self.assertEqual('JPY', jpy.name)
+
+    def test_get_rates_jpy_rate_1(self):
+        bank_1 = CbBank(datetime.date(2002, 3, 2))
+        jpy = bank_1.get_rates_jpy()
         self.assertEqual(23.1527, jpy.rate)
 
-    def test_get_rates_jpy_2(self):
+    def test_get_rates_jpy_curr_name_2(self):
         bank_2 = CbBank(datetime.date(2015, 7, 27))
         jpy = bank_2.get_rates_jpy()
         self.assertEqual('JPY', jpy.name)
+
+    def test_get_rates_jpy_rate_2(self):
+        bank_2 = CbBank(datetime.date(2015, 7, 27))
+        jpy = bank_2.get_rates_jpy()
         self.assertEqual(46.8044, jpy.rate)
 
-    def test_get_rates_jpy_3(self):
+    def test_get_rates_jpy_curr_name_3(self):
         bank_3 = CbBank(datetime.date(2020, 5, 30))
         jpy = bank_3.get_rates_jpy()
         self.assertEqual('JPY', jpy.name)
+
+    def test_get_rates_jpy_rate_3(self):
+        bank_3 = CbBank(datetime.date(2020, 5, 30))
+        jpy = bank_3.get_rates_jpy()
         self.assertEqual(65.9539, jpy.rate)
 
     def test_get_rates_jpy_error(self):
         bank_4 = CbBank(datetime.date(1950, 1, 27))
         with self.assertRaises(CentralBankError):
             bank_4.get_rates_jpy()
-
-
